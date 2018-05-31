046ed0a849f383677f4afbb43a9f14167f4361c5
YANG YOU SUNG - DevFallingstar <stardung86@gmail.com>
2018-05-31 15:31:12 +0900

    [IMPLEMENT] Add API Calling for device(video) service, profile and information

--- RestHelper/EdgeXRestHelper.py
+++ RestHelper/EdgeXRestHelper.py
@@ -16,7 +16,6 @@ class VideoInfoRestHelper:
     def __init__(self, API_HOST):
         self.API_HOST = API_HOST
         self.API_HEADERS = {'Content-Type': 'application/json'}
-        pass
 
     def __del__(self):
         pass
@@ -34,5 +33,21 @@ class VideoInfoRestHelper:
         deviceService_data = deviceService_object.getData()
         resp = requests.post(url=url, headers=self.API_HEADERS, json=deviceService_data)
         print(deviceService_data)
+        print(resp.text)
+        return resp
+
+    def post_deviceProfile(self, deviceProfile_object):
+        url = self.API_HOST + 'deviceprofile'
+        deviceProfile_data = deviceProfile_object.getData()
+        resp = requests.post(url=url, headers=self.API_HEADERS, json=deviceProfile_data)
+        print(deviceProfile_data)
+        print(resp.text)
+        return resp
+
+    def post_deviceInfo(self, deviceInfo_object):
+        url = self.API_HOST + 'device'
+        deviceInfo_data = deviceInfo_object.getData()
+        resp = requests.post(url=url, headers=self.API_HEADERS, json=deviceInfo_data)
+        print(deviceInfo_data)
         print(resp.text)
         return resp
